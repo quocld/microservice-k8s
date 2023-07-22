eb1ea8820a73690cd4b2fe0944537ee64105f473
quocld <quocle2208@gmail.com>
2023-07-22 22:53:33 +0700

    ✨ feat(reservation service): init repository
    initial reservations repository

---
+++ apps/reservations/src/repository/reservation.repository.ts
@@ -0,0 +1,17 @@
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ReservationDocument } from '../models/reservation.schema';
+
+@Injectable()
+export class ReservationRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
