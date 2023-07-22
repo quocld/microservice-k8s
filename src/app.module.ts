488bc4e033b8546a73652343681fab05ad4df1f1
quocld <quocle2208@gmail.com>
2023-07-22 21:28:08 +0700

    🐳 chore(configModule): Adding configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
